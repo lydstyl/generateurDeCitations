aa5277fcfe193fc54bb71cbf398e26f4dc4b36cc
CORP\gabrun <gabriel.brun@capgemini.com>
2017-05-05 14:46:31 +0200

    end work today 6min23 video

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render } from 'react-dom';
+import citations from '../citations';
+
+class App extends React.Component{
+    state = {}
+    componentWillMount(){
+        this.genererCitation();
+    }
+    genererCitation = event => {
+        // On transforme les citations en Array
+        const keyArray = Object.keys(citations)
+        // Une citation au hasard
+        const randomKey = keyArray[Math.floor(Math.random() * keyArray.length)]
+        if(this.state.citation === citations[randomKey].citation){
+            this.genererCitation(event)
+            return
+        }
+        this.setState(citations[randomKey])
+    }
+    render(){
+        return(
+            <div>
+                <p>
+                    {this.state.citation}
+                    <span>- {this.state.auteur}</span>
+                </p>
+                <button onClick={e => this.genererCitation(e)}>Une autre citation!</button>
+            </div>
+        )
+    }
+}
+
+render(
+    <App />,
+    document.getElementById('root')
+);
+
+export default App
---
+++ src/components/citation.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+class Citation extends React.Component{
+    render(){
+        return
+    }
+}
