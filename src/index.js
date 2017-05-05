453bff92a1dee30bb12ce736518a30fe60053592
CORP\gabrun <gabriel.brun@capgemini.com>
2017-05-05 14:32:37 +0200

    end of mini serie 05

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 // Code React
 import React from 'react';
 import { render } from 'react-dom';
+import citations from './citations';
+import './index.css';
 
 class App extends React.Component{
+    state = {}
+    componentWillMount(){
+        this.genererCitation();
+    }
+    genererCitation = event => {
+        // On transforme les citations en Array
+        const keyArray = Object.keys(citations)
+        // Une citation au hasard
+        const randomKey = keyArray[Math.floor(Math.random() * keyArray.length)]
+        if(this.state.citation === citations[randomKey].citation){
+            this.genererCitation(event)
+            return
+        }
+        this.setState(citations[randomKey])
+    }
     render(){
         return(
             <div>
-            <p>
-                Ma citation
-                <span>- Auteur</span>
-            </p>
-            <button>Une autre citation!</button>
+                <p>
+                    {this.state.citation}
+                    <span>- {this.state.auteur}</span>
+                </p>
+                <button onClick={e => this.genererCitation(e)}>Une autre citation!</button>
+            </div>
         )
     }
 }
